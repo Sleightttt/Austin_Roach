8ee62bffc9b120533841bac767330db3f16e72be
Sleightttt <brennancota@yahoo.com>
2024-02-19 22:59:01 -0800

    added shtoyleee

--- planner-app/src/App.js
+++ planner-app/src/App.js
@@ -7,10 +7,21 @@ import Projects from "./components/Projects";
 import Contract from "./components/Contract";
 import Contact from "./components/Contact";
 import "./app.css";
+import video from "./assets/video/video_preview_h264.mp4";
+import snow from "./assets/images/unsplash.jpg";
 
 function App() {
   return (
     <div className="App">
+      <div className="video-background">
+        <video autoPlay loop muted>
+          <source src={video} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <div className="video-background2">
+        <img src={snow} />
+      </div>
       <NavBar />
       <ViewBox>
         <Routes>
