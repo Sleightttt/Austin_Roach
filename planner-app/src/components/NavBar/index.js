ffbf72ad9b82d191bd9517d854caa7c43e3471be
Sleightttt <brennancota@yahoo.com>
2024-02-19 18:19:32 -0800

    working on styling

--- planner-app/src/components/NavBar/index.js
+++ planner-app/src/components/NavBar/index.js
@@ -6,19 +6,9 @@ import icons from "../../assets/icons";
 const NavBar = () => {
   return (
     <div className="nav-box">
-      <div className="logo-cont">
-        {/* Use Link component for logo-cont */}
-        <Link to="/" className="logo-link">
-          <div className="logo-text">Brennan</div>
-          <div className="logo-text">Cota</div>
-          <div>&nbsp;</div>
-          <div className="logo-text">Software</div>
-          <div className="logo-text">Engineer</div>
-        </Link>
-      </div>
       <div className="nav-cont">
         <Link to="/about" className="icon-cont">
-          <img className="nav-icon" src={icons.blueprint} alt="Projects" />
+          <img className="nav-icon" src={icons.information} alt="Projects" />
           <div className="logo-text">About Me</div>
         </Link>
         <Link to="/projects" className="icon-cont">
