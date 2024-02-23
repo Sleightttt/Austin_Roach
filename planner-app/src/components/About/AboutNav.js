13452dd360d920d5ff75632a9ffb7bf3041aa154
Sleightttt <brennancota@yahoo.com>
2024-02-22 21:25:09 -0800

    fixed inital load of titles in about

--- planner-app/src/components/About/AboutNav.js
+++ planner-app/src/components/About/AboutNav.js
@@ -9,6 +9,7 @@ const AboutNav = () => {
   ];
   const [currentTitleIndex, setCurrentTitleIndex] = useState(0);
   const [fadeIn, setFadeIn] = useState(false); // Initially set to false
+  const [initialLoad, setInitialLoad] = useState(true);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -17,17 +18,29 @@ const AboutNav = () => {
         setCurrentTitleIndex((prevIndex) => (prevIndex + 1) % titles.length);
         setFadeIn(true); // Start fading in with the new title
       }, 500); // Wait for 500ms before changing the title
-    }, 3000); // Change the title every 3 seconds (3000 milliseconds)
+    }, 4000); // Change the title every 3 seconds (3000 milliseconds)
+
+    // Disable initial sliding animation after the first load
+    if (initialLoad) {
+      setTimeout(() => {
+        setInitialLoad(false);
+        setFadeIn(true); // Start fading in the first title after sliding animation
+      }, 1000); // Adjust this timeout to match the sliding animation duration
+    }
 
     return () => clearInterval(intervalId); // Clean up the interval
-  }, []);
+  }, [initialLoad]);
 
   return (
     <div className="aboutnav-cont">
-      <h1 className={`name-h slide-right ${fadeIn ? "fade-in" : "fade-out"}`}>
+      <h1 className={`name-h slide-left ${fadeIn ? "fade-in" : "fade-out"}`}>
         Brennan Cota
       </h1>
-      <h1 className={`name-h ${fadeIn ? "fade-in" : "fade-out"}`}>
+      <h1
+        className={`name-h2 ${initialLoad ? "slide-right" : ""} ${
+          fadeIn ? "fade-in" : "fade-out"
+        }`}
+      >
         {titles[currentTitleIndex]}
       </h1>
     </div>
