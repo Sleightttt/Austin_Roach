da96f1189f663cb2fef6535bb294978a346acd9a
Sleightttt <brennancota@yahoo.com>
2024-03-12 22:52:15 -0700

    added clarity to download links

--- planner-app/src/components/Projects/index.js
+++ planner-app/src/components/Projects/index.js
@@ -27,8 +27,7 @@ const Projects = () => {
 
   const entryContent = {
     image: {
-      about:
-        "Imagespace is a Flickr clone with 5 CRUD features built using a React frontend with a Flask backend. Imagespace features an auto-tagging feature by implementing the AI image recognition API Imagga. Users can sign up for accounts, follow and unfollow other users, both like and comment on photos, and upload their own! Imagespace also features a search bar that allows users to search for other users, images, and tags. Users can also edit their own profiles and delete their own images and comments. This website was built with a 'mobile first' approach, and is fully responsive for mobile, tablet, and desktop.",
+      about: `Imagespace is a Full Stack Flickr clone with 5 CRUD features built using a React frontend with a Flask backend. Imagespace features an auto-tagging feature by implementing the AI image recognition API Imagga. Users can sign up for accounts, follow and unfollow other users, both like and comment on photos, and upload their own! Imagespace also features a search bar that allows users to search for other users, images, and tags. Users can also edit their own profiles and delete their own images and comments. This website was built with a 'mobile first' approach, and is fully responsive for mobile, tablet, and desktop.`,
       githubLink: "https://github.com/Sleightttt/AA-Captstone-Project",
       techIcons: [
         "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-original-wordmark.svg",
